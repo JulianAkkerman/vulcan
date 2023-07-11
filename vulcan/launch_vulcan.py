456fbbfad10753df715ef175b0cf347932cf3106
jgroschwitz <jonasg@coli.uni-saarland.de>
2023-07-11 15:00:30 +0200

    added some documentation

--- vulcan/launch_vulcan.py
+++ vulcan/launch_vulcan.py
@@ -13,18 +13,24 @@ from amconll import parse_amconll
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("pickle_filename", help="Path to the pickle file you want to visualize.")
+    parser.add_argument("pickle_filename", help="Path to the pickle file you want to visualize. You can specify"
+                                                "a JSON file instead if you use the --json flag.")
     parser.add_argument("-pf", "--propbank-frames", type=str,
                         action="store", dest="propbank_frames", default=None,
                         help="Path to a folder containing XML files with Propbank frames, "
                              "such as data/frames/propbank-amr-frames-xml-2018-01-25 in the AMR3.0 corpus. "
-                             "If given, vulcan will show frame definitions on node mouseover ")
+                             "If given, vulcan will show frame definitions on node mouseover (only works for "
+                             "propbank frames; designed to be used with AMR graphs)")
     parser.add_argument("-wiki", "--show-wikipedia-articles", action="store_true", dest="show_wikipedia_articles",
-                        default=False, help="If given, vulcan will show Wikipedia articles on node mouseover."
+                        default=False, help="If given, vulcan will show Wikipedia articles for AMR graphs on node mouseover."
                                             "Can take quite a while to load in the beginning!")
-    parser.add_argument("-p", "--port", type=int, action="store", dest="port", default=5050)
-    parser.add_argument("--json", action="store_true", dest="is_json_file", default=False)
-    parser.add_argument("--show_node_names", action="store_true", dest="show_node_names", default=False)
+    parser.add_argument("-p", "--port", type=int, action="store", dest="port", default=5050,
+                        help="Specify the port to use for this visualization.")
+    parser.add_argument("--json", action="store_true", dest="is_json_file", default=False,
+                        help="Use this flag if your input file is a JSON file rather than a pickle.")
+    parser.add_argument("--show-node-names", action="store_true", dest="show_node_names", default=False,
+                        help="A graph or tree node with name `n` and label `label` will be shown as"
+                             " `n / label` (per default, only `label` is shown).")
     args = parser.parse_args()
 
     if args.propbank_frames is not None:
